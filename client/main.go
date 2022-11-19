29627a14bd1804f700122b71fb41cd3c8459cc8a
Emjakobsen <erja@itu.dk>
2022-11-19 17:17:02 +0100

    3 servers work in progress

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 	"time"
@@ -10,29 +11,60 @@ import (
 	"google.golang.org/grpc"
 )
 
+var bidderName = flag.String("name", "Anonymous user", "type -name <yourname> to set name of the bidder.")
+var servers []gRPC.AuctionhouseClient
+var ServerConn map[gRPC.AuctionhouseClient]*grpc.ClientConn
+var _ports = [3]string{"9080", "9081", "9082"}
+
 func main() {
-	// Creat a virtual RPC Client Connection on port  9080 WithInsecure (because  of http)
+	flag.Parse()
+	ServerConn = make(map[gRPC.AuctionhouseClient]*grpc.ClientConn)
 	var conn *grpc.ClientConn
+
 	conn, err := grpc.Dial(":9080", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not connect: %s", err)
 	}
-
 	// Defer means: When this function returns, call this method (meaing, one main is done, close connection)
 	defer conn.Close()
-	c := gRPC.NewAuctionhouseClient(conn)
-
-	for {
-		SendVoid(c)
-		time.Sleep(5 * time.Second)
+	//c := gRPC.NewAuctionhouseClient(conn)
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithBlock(), grpc.WithInsecure())
+	timeContext, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	for _, port := range _ports {
+		log.Printf("client %s: Attempts to dial on port %s\n", *bidderName, port)
+		conn, err := grpc.DialContext(timeContext, fmt.Sprintf(":%s", port), opts...)
+		if err != nil {
+			log.Printf("Fail to Dial : %v", err)
+			continue
+		}
+		var s = gRPC.NewAuctionhouseClient(conn)
+		servers = append(servers, s)
+		ServerConn[s] = conn
+		fmt.Println(conn.GetState().String())
 	}
+	/*
+		for i := 0; i < 10; i++ {
+			Bid(c, (int32(i)))
+			Result(c)
+			time.Sleep(5 * time.Second)
+		}*/
 
 }
-func SendVoid(c gRPC.AuctionhouseClient) {
+func Result(c gRPC.AuctionhouseClient) {
 	msg := gRPC.Void{}
 	response, err := c.Result(context.Background(), &msg)
 	if err != nil {
 		log.Fatalf("Error calling Result RPC: %s ", err)
 	}
 	fmt.Printf("%s Highest bid: %d \n", response.Status, response.HighestBid)
+}
+func Bid(c gRPC.AuctionhouseClient, amountToBid int32) {
+	msg := gRPC.Amount{Bidder: *bidderName, Amount: amountToBid}
+	response, err := c.Bid(context.Background(), &msg)
+	if err != nil {
+		log.Fatalf("error calling Bid RPC %s ", err)
+	}
+	fmt.Printf("-- %s \n", response.Status)
 }
--- server/main.go
+++ server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net"
 
@@ -9,38 +11,74 @@ import (
 	"google.golang.org/grpc"
 )
 
+var server *Server
+var duration = flag.Int("n", 120, "duration for the auction.")
+var ports = [3]string{"9080", "9081", "9082"}
+
 type Server struct {
 	// an interface that the server needs to have
 	gRPC.UnimplementedAuctionhouseServer
+	port              string
 	currentHighestBid int32
+	running           bool
 	// here you can impliment other fields that you want
 }
 
-var server *Server
-
 func (s *Server) Bid(ctx context.Context, Amount *gRPC.Amount) (*gRPC.Ack, error) {
-	ack := &gRPC.Ack{Status: "Bid/ended"}
-	return ack, nil
+	log.Printf("Current highest bid %d \n", s.currentHighestBid)
+	if Amount.Amount > s.currentHighestBid {
+		s.currentHighestBid = Amount.Amount
+		ack := &gRPC.Ack{Status: "SUCCESS, amount:" + fmt.Sprint(Amount.Amount) + " bidder: " + Amount.Bidder}
+		return ack, nil
+	} else {
+		ack := &gRPC.Ack{Status: "FAIL: must be higher than current highest bid. " + Amount.Bidder}
+		return ack, nil
+
+	}
+
 }
 
 func (s *Server) Result(ctx context.Context, Void *gRPC.Void) (*gRPC.Outcome, error) {
-	outcome := &gRPC.Outcome{Status: "Result/ended", HighestBid: s.currentHighestBid}
+	outcome := &gRPC.Outcome{Status: "RUNNING", HighestBid: s.currentHighestBid}
 	return outcome, nil
 }
 
 func main() {
-	launchServer()
+	flag.Parse()
+	log.Printf("AUCTION started with duration: %d", *duration)
+	go launch(ports[0])
+	go launch(ports[1])
+	launch(ports[2])
+
 }
 
-func launchServer() {
-	list, err := net.Listen("tcp", "localhost:9080")
+func getServer(serverPort string) *Server {
+	s := &Server{
+		port:              serverPort,
+		currentHighestBid: 0,
+		running:           true,
+	}
+	log.Println(s)
+	return s
+
+}
+
+func launch(portAddress string) {
+
+	list, err := net.Listen("tcp", "localhost:"+portAddress)
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9080: %v", err)
 		return
 	}
 	grpcServer := grpc.NewServer()
-	gRPC.RegisterAuctionhouseServer(grpcServer, &Server{currentHighestBid: 0})
-
+	server = getServer((portAddress))
+	gRPC.RegisterAuctionhouseServer(grpcServer, server)
+	log.Printf("Server running on port %s", server.port)
+	/*
+		time.Sleep(time.Second * time.Duration(*duration))
+		server.running = false
+		log.Println("AUCTION has ended")
+		//os.Exit(3)*/
 	if err := grpcServer.Serve(list); err != nil {
 		log.Fatalf("failed to server %v", err)
 	}
